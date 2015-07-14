fa03f0a33b58b5a69451d1ec4f9f3c51a16f5823
Leonard Ngeno <dranoel.bravo@gmail.com>
2015-07-14 11:06:55 -0400

    add instrument and project links to response exports on the admin side

--- app/admin/response_export.rb
+++ app/admin/response_export.rb
@@ -10,6 +10,30 @@ ActiveAdmin.register ResponseExport do
     link_to 'Download Files', download_exports_admin_response_export_path(response_export)
   end
 
+  index do
+    selectable_column
+    column :id do |export|
+      link_to export.id, admin_response_export_path(export.id)
+    end
+    column 'Instrument', sortable: :instrument_title do |export|
+      instrument = Instrument.find_by_id(export.instrument_id) if export.instrument_id
+      instrument ? (link_to instrument.title, admin_instrument_path(instrument)) : ''
+    end
+    column 'Project', sortable: :name do |export|
+      project = Project.find_by_id(export.project_id) if export.project_id
+      project ? (link_to project.name, admin_project_path(project)) : ''
+    end
+    column :instrument_versions do |export|
+      export.instrument_versions.join(',') if export.instrument_versions
+    end
+    column :long_done
+    column :wide_done
+    column :short_done
+    column :created_at
+    column :updated_at
+    actions
+  end
+
   controller do   
     def download_exports
       response_export = ResponseExport.find_by_id(params[:id])
