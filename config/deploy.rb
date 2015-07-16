47f0abe4e71a036e4223017d8efcbcaee86ed93f
Leonard Ngeno <dranoel.bravo@gmail.com>
2015-07-16 16:15:53 -0400

    setup sidekiq monitoring

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :linked_dirs, fetch(:linked_dirs) + %w{ files updates }
 set :branch, 'master'
 set :sidekiq_pid, File.join(shared_path, 'tmp', 'pids', 'sidekiq.pid')
 set :sidekiq_log, File.join(shared_path, 'log', 'sidekiq.log')
-set :sidekiq_concurrency, 25
+set :sidekiq_concurrency, 10
 set :sidekiq_processes, 2
 
 namespace :deploy do
@@ -38,17 +38,24 @@ namespace :deploy do
     end 
   end
   
-  desc "Moniter redis"
+  desc "Monitor redis"
   task :config_redis do
     on roles(:app) do
       execute "sudo mv #{release_path}/config/deploy/shared/redis.erb /etc/monit/conf.d/redis_rails_survey.conf"
     end
   end 
   
-  desc "Moniter apache2"
+  desc "Monitor apache2"
   task :config_apache2 do
     on roles(:app) do
       execute "sudo mv #{release_path}/config/deploy/shared/apache2.erb /etc/monit/conf.d/apache2_rails_survey.conf"
+    end
+  end
+
+  desc 'Monitor Sidekiq'
+  task :config_sidekiq do
+    on roles(:app) do
+      execute "sudo mv #{release_path}/config/deploy/shared/sidekiq.erb /etc/monit/conf.d/sidekiq_rails_survey.conf"
     end
   end
   
@@ -65,5 +72,6 @@ namespace :deploy do
   after 'deploy:published', 'sidekiq:monit:config'
   after 'deploy:published', 'deploy:config_redis'
   after 'deploy:published', 'deploy:config_apache2'
+  after 'deploy:published', 'deploy:config_sidekiq'
   after 'deploy:published', 'deploy:restart_monit'
 end
