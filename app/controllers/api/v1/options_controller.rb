ba72262010511710553f2ff6314a6feef95f1700
Leonard Ngeno <dranoel.bravo@gmail.com>
2015-07-24 16:56:30 -0400

    select objects to sync based on last sync time

--- app/controllers/api/v1/options_controller.rb
+++ app/controllers/api/v1/options_controller.rb
@@ -1,11 +1,12 @@
 module Api
   module V1
     class OptionsController < ApiApplicationController
+      include Syncable
       respond_to :json
 
       def index
         project = Project.find(params[:project_id])
-        options = project.options_to_sync(project.ids_and_versions(params[:device_instruments], params[:device_instrument_versions]))
+        options = to_sync(project.options, 'options', params[:last_sync_time])
         respond_with options, include: :translations
       end
 
