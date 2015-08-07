4a71295c822b1fe471dd640ee43c3f76a911f7fe
Leonard Ngeno <dranoel.bravo@gmail.com>
2015-08-07 17:14:57 -0400

    Add Material Responsive theme - step 1

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,9 @@ module RailsSurvey
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.paths << Rails.root.join("vendor", "assets", "images")
-    config.assets.paths << Rails.root.join("vendor", "assets", "lib")
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'lib')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
     I18n.enforce_available_locales = false
     config.assets.initialize_on_precompile = false
   end
