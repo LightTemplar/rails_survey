c4f60df1a9b6783b9980cbaf81898ddf8fdde199
Leonard Ngeno <dranoel.bravo@gmail.com>
2015-08-21 14:34:38 -0400

    optimize sections, skips and rules sync time

--- app/controllers/api/v1/rules_controller.rb
+++ app/controllers/api/v1/rules_controller.rb
@@ -1,11 +1,13 @@
 module Api
   module V1
     class RulesController < ApiApplicationController
+      include Syncable
       respond_to :json
 
       def index
         project = Project.find(params[:project_id])
-        respond_with project.rules.with_deleted
+        rules = to_sync(project.rules, 'rules', params[:last_sync_time])
+        respond_with rules
       end
     end 
   end
--- app/controllers/api/v1/skips_controller.rb
+++ app/controllers/api/v1/skips_controller.rb
@@ -1,14 +1,19 @@
 module Api
   module V1
     class SkipsController < ApiApplicationController
+      include Syncable
       respond_to :json
+
       def index
         project = Project.find(params[:project_id])
-        respond_with project.skips.with_deleted
+        skips = to_sync(project.skips, 'skips', params[:last_sync_time])
+        respond_with skips
       end
+
       def show
         respond_with Skip.find(params[:id])
       end
+
     end 
   end
 end
