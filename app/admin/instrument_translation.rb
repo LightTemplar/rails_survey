c2dee19b8906f19720855dd3b15069c5ace8e5ed
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-09-22 15:08:17 -0400

    activeadmin override default collection name for foreign key based models

--- app/admin/instrument_translation.rb
+++ app/admin/instrument_translation.rb
@@ -1,7 +1,6 @@
 ActiveAdmin.register InstrumentTranslation do
   belongs_to :instrument
   permit_params :title, :language, :alignment, :instrument_id
-  config.per_page = 10
 
   controller do
     defaults :collection_name => "translations"
