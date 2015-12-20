d568a696be3797dd17ef69439f4e9d4dccb17d50
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-12-20 14:57:26 +0300

    test

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+if ENV["TMPDIR"] && ENV["TMPDIR"].index("passenger")
+  std_out = File.new(RAILS_ROOT + "/log/stdout.log","a")
+  std_err = File.new(RAILS_ROOT + "/log/stderr.log","a")
+  $stdout.reopen(std_out)
+  $stderr.reopen(std_err)
+end
+
 # Initialize the Rails application.
 RailsSurvey::Application.initialize!
