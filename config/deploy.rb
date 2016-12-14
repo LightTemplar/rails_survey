fca97212f01a56acb3a3389e4986fcde25b84396
Leonard Ng'eno <dranoel.bravo@gmail.com>
2016-12-14 14:32:57 -0500

    configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,85 +19,20 @@ set :sidekiq_log, File.join(shared_path, 'log', 'sidekiq.log')
 set :sidekiq_concurrency, 15
 set :sidekiq_processes, 2
 
-set :puma_init_active_record, true
-set :puma_user, fetch(:user)
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
-set :puma_access_log, "#{shared_path}/log/puma_access.log"
-set :puma_error_log, "#{shared_path}/log/puma_error.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 32]
-set :puma_workers, 4
-set :puma_worker_timeout, nil
-set :puma_preload_app, true
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-# When using Puma App Server
-namespace :deploy do
-  desc 'Make sure local git is in sync with remote.'
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts 'WARNING: HEAD is not the same as origin/master'
-        puts 'Run `git push` to sync changes.'
-        exit
-      end
-    end
-  end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting, :check_revision
-  after :finishing, :compile_assets
-  after :finishing, :cleanup
-  after :finishing, :restart
-end
-
-#TODO Add script to restart Puma & Nginx on server reboot
-
 # When using Phusion Passenger App Server
-# namespace :deploy do
-#   desc 'Restart Application'
-#   task :restart do
-#     desc 'restart phusion passenger'
-#     on roles(:app), in: :sequence, wait: 5 do
-#       execute :touch, current_path.join('tmp/restart.txt')
-#     end
-#   end
-#
-#   after :finishing, 'deploy:cleanup'
-#   after 'deploy:publishing', 'deploy:restart'
-#   after 'deploy:published', 'sidekiq:monit:config'
-# end
+namespace :deploy do
+  desc 'Restart Application'
+  task :restart do
+    desc 'restart phusion passenger'
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, current_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+  after 'deploy:publishing', 'deploy:restart'
+  after 'deploy:published', 'sidekiq:monit:config'
+end
 
 namespace :clients do
   task :deploy_on_all do
