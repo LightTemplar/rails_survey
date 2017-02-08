60dcb8e2fb65cc5fd70ebd1fc2abe9739079dd07
Leonard Ng'eno <dranoel.bravo@gmail.com>
2017-02-08 12:47:48 -0500

    add scheme edit action, score weights

--- app/controllers/score_schemes_controller.rb
+++ app/controllers/score_schemes_controller.rb
@@ -1,10 +1,20 @@
 class ScoreSchemesController < ApplicationController
+  before_action :set_scheme, only: [:show, :edit, :update, :destroy]
+
   def index
     @schemes = current_project.score_schemes
   end
 
-  def show
-    @scheme = current_project.score_schemes.find params[:id]
+  def show; end
+
+  def edit; end
+
+  def update
+    if @scheme.update(scheme_params)
+      redirect_to project_score_scheme_path(current_project, @scheme), notice: 'Score scheme was successfully updated'
+    else
+      render action: :edit
+    end
   end
 
   def score
@@ -12,5 +22,15 @@ class ScoreSchemesController < ApplicationController
     scheme = current_project.score_schemes.find(params[:id])
     scheme.score_survey(survey)
     redirect_to project_scores_path
+  end
+
+  private
+
+  def set_scheme
+    @scheme = current_project.score_schemes.find params[:id]
+  end
+
+  def scheme_params
+    params.require(:score_scheme).permit(:title)
   end
 end
