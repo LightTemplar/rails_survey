5df4265cebce2a10fa4f275ac523da38d25946f8
Leonard Ng'eno <dranoel.bravo@gmail.com>
2018-03-20 15:31:45 -0400

    add convenience links for options

--- app/controllers/api/v2/questions_controller.rb
+++ app/controllers/api/v2/questions_controller.rb
@@ -4,7 +4,7 @@ module Api
       respond_to :json
 
       def index
-        if params[:instrument_id]
+        if !params[:instrument_id].blank?
           instrument = Instrument.find params[:instrument_id]
           respond_with instrument.questions
         else
