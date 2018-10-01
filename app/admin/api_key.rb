c1c8b9623699e468e7aef09c89edfddee0b4fa9c
Leonard Ng'eno <dranoel.bravo@gmail.com>
2018-10-01 15:01:44 -0400

    remove some admin template views

--- app/admin/api_key.rb
+++ app/admin/api_key.rb
@@ -1,27 +1,27 @@
-ActiveAdmin.register ApiKey do
-  permit_params :access_token
-
-  index do
-    column :id
-    column :access_token
-    actions
-  end
-
-  show do |_key|
-    attributes_table do
-      row :id
-      row :access_token
-    end
-
-    active_admin_comments
-  end
-
-  form do |f|
-    unless f.object.new_record?
-      f.inputs 'Api Key Details' do
-        f.input :access_token
-      end
-    end
-    f.actions
-  end
-end
+# ActiveAdmin.register ApiKey do
+#   permit_params :access_token
+#
+#   index do
+#     column :id
+#     column :access_token
+#     actions
+#   end
+#
+#   show do |_key|
+#     attributes_table do
+#       row :id
+#       row :access_token
+#     end
+#
+#     active_admin_comments
+#   end
+#
+#   form do |f|
+#     unless f.object.new_record?
+#       f.inputs 'Api Key Details' do
+#         f.input :access_token
+#       end
+#     end
+#     f.actions
+#   end
+# end
--- app/admin/role.rb
+++ app/admin/role.rb
@@ -1,19 +1,19 @@
-ActiveAdmin.register Role do
-  menu :if => proc{ current_user.super_admin? }
-  permit_params :name
-
-  show do |role|
-    attributes_table do
-      row :id
-      row :name
-      row :created_at
-      row :updated_at
-      row 'Users' do
-        role.users.each do |user|
-          li { user.email }
-        end
-      end
-      active_admin_comments
-    end
-  end
-end
+# ActiveAdmin.register Role do
+#   menu :if => proc{ current_user.super_admin? }
+#   permit_params :name
+#
+#   show do |role|
+#     attributes_table do
+#       row :id
+#       row :name
+#       row :created_at
+#       row :updated_at
+#       row 'Users' do
+#         role.users.each do |user|
+#           li { user.email }
+#         end
+#       end
+#       active_admin_comments
+#     end
+#   end
+# end
