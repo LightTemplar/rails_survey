3c8f02ca23e3d86b362b84d3d3c7098c22c57215
Leonard Ng'eno <dranoel.bravo@gmail.com>
2018-11-11 23:27:08 -0500

    diff sync multiple/next questions

--- app/controllers/api/v3/multiple_skips_controller.rb
+++ app/controllers/api/v3/multiple_skips_controller.rb
@@ -1,10 +1,11 @@
 module Api
   module V3
     class MultipleSkipsController < Api::V1::ApiApplicationController
+      include Syncable
       respond_to :json
       def index
         @project = Project.find(params[:project_id])
-        @multiple_skips = @project.multiple_skips
+        @multiple_skips = to_sync(@project.multiple_skips, 'multiple_skips', params[:last_sync_time])
       end
     end
   end
